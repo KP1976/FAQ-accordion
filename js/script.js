cd547441b8dd544c867645e6d5b4b93d9661ab38
Krzysztof Pawłowski <kpawlowski1976@gmail.com>
2024-10-17 20:53:19 +0200

    Show answer after click on question.

--- js/script.js
+++ js/script.js
@@ -1,13 +1,18 @@
 const accordionItem = document.querySelectorAll(".accordion__item");
 
-// Change plus icon to minus icon on click.
-
 accordionItem.forEach((item) => {
   item.addEventListener("click", (e) => {
     const accordionAnswer = item.querySelector(".accordion__answer");
 
+    accordionItem.forEach((otherItem) => {
+      if (otherItem !== item) {
+        otherItem.classList.remove("open");
+        otherItem.querySelector(".accordion__answer").classList.remove("open");
+      }
+    });
+
     item.classList.toggle("open");
-    // accordionAnswer.classList.toggle("open");
+    accordionAnswer.classList.toggle("open");
 
     console.log(accordionAnswer);
   });
